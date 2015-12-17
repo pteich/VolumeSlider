75805ff620504c4ee11920eac9f3f0862ec89f55
Peter Teich <peter.teich@motor.de>
2015-12-17 11:02:59 +0100

    added two parameters to createVolumeSlider() JS to provide color of minimum slider and maximum slider

--- www/VolumeSlider.js
+++ www/VolumeSlider.js
@@ -2,7 +2,7 @@
 //	VolumeSlider.js
 //	Volume Slider Cordova Plugin
 //
-//	Created by Tommy-Carlos Williams on 20/07/11. 
+//	Created by Tommy-Carlos Williams on 20/07/11.
 //  Updated by Samuel Michelot (orbitaloop) 05/11/2013
 //	Copyright 2011 Tommy-Carlos Williams. All rights reserved.
 //      MIT Licensed
@@ -16,10 +16,10 @@ module.exports = {
     /**
 	 * Create a volume slider, and save the user's current set volume.
 	 */
-	createVolumeSlider : function(originx,originy,width,height) {
-		exec(null, null, "VolumeSlider","createVolumeSlider", [originx, originy, width, height]);
-	},
-	
+	createVolumeSlider : function(originx,originy,width,height,colorMinimum,colorMaximum) {
+ 		exec(null, null, "VolumeSlider","createVolumeSlider", [originx, originy, width, height, colorMinimum, colorMaximum]);
+ 	},
+
 	/**
 	 * Show the volume slider
 	 */
